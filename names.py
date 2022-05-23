25525c6ee4ecb83897286baab5bc85dff76c9dce
OGE-Account <v.engelowski@gmail.com>
2022-05-23 15:10:56 +0200

    Added newline at end of file

---
+++ names.py
@@ -0,0 +1,18 @@
+def get_names():
+    _dict = {
+        'Anton Häusler': 'Anton',
+        'Jan Nowak': 'Jan',
+        'Stephan Alexander Jacobs': 'Stephan',
+        'Dogus Ergüzel': 'Dogus',
+        'Paul Lysakowski': 'Paul',
+        'Immanuel Weber': 'Immanuel',
+        'Ulrich Letmathe': 'Uli',
+        'Michael Morosov': 'Michael',
+        'Marvin Stamm': 'Marvin',
+        'Björn Schmäing': 'Björn',
+        'Sebastian Wolters': 'Sebastian',
+        'Ismail Deliormanli': 'Ismail',
+        'Quang Dao Vu Ngoc': 'Dao'
+    }
+
+    return _dict
